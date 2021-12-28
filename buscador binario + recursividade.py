e3b85765b4830e306f2937ffd0bed00ec9a62ab4
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2021-12-28 11:38:37 -0300

    Update buscador binario + recursividade.py

--- buscador binario + recursividade.py
+++ buscador binario + recursividade.py
@@ -5,11 +5,14 @@ para a resoluação de um conjunto de problemas n menores dentro de un conjunto 
     if segunda_metade is None:
         segunda_metade = len(lista)-1
     if segunda_metade < primeira_metade:
-        return -1
+        return -1 # busca não encontrada dentro de um conjunto (lista) ordenado.
     else:
+        # execute isso até o momento que as duas verificações abaixo seja falsa e retorne meio.
         meio = (primeira_metade + segunda_metade)//2
     if elemento < lista[meio]:
         return buscaBinaria_recursiva(lista, elemento, primeira_metade,meio-1)
+        # caso o elemento esteja na primeira metade, então chamar a função contendo os parâmetros
+        # obs: o parametro meio-1, serve para atribuir um valor para segunda metade de tal forma que seja o antecessor do meio
     if elemento > lista[meio]:
         return buscaBinaria_recursiva(lista, elemento, meio+1, segunda_metade)
     else:
@@ -27,7 +30,10 @@ import pytest
     (a, 70, 6),
     (a, 80, 7),
     (a, 90, 8),
-    (a, 100, 9)
+    (a, 100, 9),
+    (a, 0, -1),
+    (a, 75, -1),
+    (a, -10, -1)
 ])
 def testar_buscaBinaria_recursiva(entrada, elemento, saida):
     assert buscaBinaria_recursiva(entrada, elemento) == saida
